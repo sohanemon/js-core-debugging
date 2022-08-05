2c8fd0a134592d5ac6854fb4ccd3f91357d10bd7
sohanemon <m.sohan.rse@gmail.com>
2022-08-05 14:33:56 +0600

    miles2km

--- milesToKm.js
+++ milesToKm.js
@@ -1,4 +1,20 @@
-function milesToKilometer(miles){
-    const kilometer = miles*1.609;
-    return kilometer;
-}
+/* -------------------------------------------------------------------------- */
+/*                         todo: mile to km conversion                        */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * we know 1 mile = 1.609 km
+ * 1 km = 0.62 mile
+ * lets start the program
+ */
+
+const mile2km = (mile) => {
+  return (mile * 1.609).toFixed(3);
+};
+const km2mile = (km) => {
+  return (km * 0.62).toFixed(3);
+};
+
+const myWay = 10;
+console.log(mile2km(myWay));
+console.log(km2mile(myWay));
